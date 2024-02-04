b98ac9ef699d50dabe3fcde947e273eaea00091f
Sergii Myronenko <sergii.myronenko@googlemail.com>
2024-02-04 20:58:42 +0200

    final
    ready for submission

--- js/task-2.js
+++ js/task-2.js
@@ -1,44 +1,30 @@
-function calcAverageCalories(days) {
-let total = 0;
-let average = 0;
 
-for(const day of days){
-    total += day.calories;
-  } 
-  if(days.length === 0){;
-     return 0;
+class Storage {
+#items = [];
+
+constructor(items) {
+    this.#items = items;
+  }
+
+getItems() {
+    return this.#items;
 }
 
-else{average = total / days.length;}
+addItem(newItem) {
+    this.#items.push(newItem);
+}
 
-return average;
-    } 
+removeItem(itemToRemove){
+    
+    if(this.#items.indexOf(itemToRemove) > -1) {this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
+}
 
+}
 
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 3010 },
-      { day: "tuesday", calories: 3200 },
-      { day: "wednesday", calories: 3120 },
-      { day: "thursday", calories: 2900 },
-      { day: "friday", calories: 3450 },
-      { day: "saturday", calories: 3280 },
-      { day: "sunday", calories: 3300 }
-    ])
-  ); // 3180
-
-  console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 2040 },
-      { day: "tuesday", calories: 2270 },
-      { day: "wednesday", calories: 2420 },
-      { day: "thursday", calories: 1900 },
-      { day: "friday", calories: 2370 },
-      { day: "saturday", calories: 2280 },
-      { day: "sunday", calories: 2610 }
-    ])
-  ); // 2270
-  
-  console.log(
-    calcAverageCalories([])
-  ); // 0
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
